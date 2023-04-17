3f9eb14b4b6ae756f8501fb0e2a8dd2c490085c6
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2023-04-17 20:05:13 -0300

    Variaveis - Pratica

---
+++ scripts/variaveis.js
@@ -0,0 +1,20 @@
+//String: responsavel por guardar caracteres e textos, atribuir com aspas simples ou uplas
+
+var texto = 'Curso de Javascript'
+
+//Number valores numericos, negativos, positivos, fracionados e etc.
+
+var numeroInteriro = -7
+
+var numeroFracionado = 10.75
+
+
+//Boolean: Condicionais
+
+var teste = true // true = verdadeiro, false = falso
+
+//alert(teste) //abrir um dialog
+
+//document.write(texto) // escreve algo no site(por exemplo)
+
+console.log(numeroInteriro)
