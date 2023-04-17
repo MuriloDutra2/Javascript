bdc3df6b20cfcb58f96410c30dd6e0b500bac05e
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2023-04-17 20:17:47 -0300

    Concatenação

---
+++ scripts/concatenacao.js
@@ -0,0 +1,10 @@
+var nome = prompt('Digite o seu nome')
+
+var idade = prompt('Digite a sua idade')
+
+var idade2 = 52
+
+console.log(idade)
+console.log(idade2)
+
+document.write('<h1>Olá ' + nome + ', tudo bem? Estou vendo aqui que você possui ' + idade + ' anos </h1>')
