e3f5c17c1866925961616894fb44e31af84fa8d5
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2023-04-19 13:04:04 -0300

    Desafio 1/ concluido

---
+++ scripts/desafio1.js
@@ -0,0 +1,32 @@
+var a = 10
+
+var b = 20
+
+var c = null //auxiliar na logica
+
+document.write(
+    'A: ' + a + '<br>' +
+    'B: ' + b + '<br>' +
+    'C: ' + c + '<br>'
+)
+
+
+//logica de inversaão dos valores das variaveis
+
+c = a
+
+a = b
+
+b = c
+
+c = null
+
+
+
+document.write(
+    'A: ' + a + '<br>' +
+    'B: ' + b + '<br>' +
+    'C: ' + c + '<br>'
+)
+
+
