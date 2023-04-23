470a71359ac2e3a5517f0c1e79c67f1b3c302bd1
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2023-04-23 08:47:20 -0300

    Operadores if e else

---
+++ scripts/operadores_ifelse.js
@@ -0,0 +1,55 @@
+var murilo = 2
+var joao = 3
+
+//igual
+if( murilo == 2) {
+    document.write('Entrou no bloco if (verdadeiro) <br>')
+} else {
+    document.write('Entrou no bloco else (falso) <br>')
+}
+
+
+//diferente
+if( joao != 2) {
+    document.write('Entrou no bloco if (verdadeiro) <br>')
+} else {
+    document.write('Entrou no bloco else (falso) <br>')
+}
+
+//não identico
+if( "2" !== 2) {
+    document.write('Entrou no bloco if (verdadeiro) <br>')
+} else {
+    document.write('Entrou no bloco else (falso) <br> ')
+}
+
+// menor que
+
+if( 8 < 2) {
+    document.write('Entrou no bloco if (verdadeiro) <br>')
+} else {
+    document.write('Entrou no bloco else (falso) <br> ')
+}
+
+//maior que
+
+if( 8 > 2) {
+    document.write('Entrou no bloco if (verdadeiro) <br>')
+} else {
+    document.write('Entrou no bloco else (falso) <br> ')
+}
+
+//maior igual
+
+if(5 <= 22) {
+    document.write('Entrou no bloco if (verdadeiro) <br>')
+} else {
+    document.write('Entrou no bloco else (falso) <br> ')
+}
+
+//menor igual
+if(5 >= 22) {
+    document.write('Entrou no bloco if (verdadeiro) <br>')
+} else {
+    document.write('Entrou no bloco else (falso) <br> ')
+}
