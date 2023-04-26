a816dd33dcdc6cb7d5cbe429318a6b88be665290
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2023-04-26 08:07:21 -0300

    Casting

---
+++ scripts/casting.js
@@ -0,0 +1,23 @@
+
+
+//De valor String para numerico
+/*
+var variavel1 = prompt('Digite um numero')
+var variavel2 = prompt('Digite outro numero')
+variavel1 = parseInt (variavel1) 
+não preserva a fração
+variavel2 = parseFloat (variavel2) 
+preserva a fração
+
+document.write(variavel1 + variavel2)*/
+
+//De valor Numerico para String
+
+var variavel1 = 20
+var variavel2 = 30
+
+
+console.log(variavel1)
+console.log(variavel2)
+
+document.write(variavel1.toString() + variavel2.toString())
