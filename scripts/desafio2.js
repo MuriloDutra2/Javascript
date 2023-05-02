cba47efbe38eefdcca44a0de84c34314984d1de4
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2023-05-02 06:00:09 -0300

    Desafio 2

---
+++ scripts/desafio2.js
@@ -0,0 +1,40 @@
+
+//feito por mim
+var idade = prompt('Qual é a sua idade?')
+
+if(idade <= 15 ) {
+    document.write(' Você é uma criança')
+} 
+
+if(idade <= 30) {
+    document.write(' Você é um jovem')
+} 
+
+if(idade < 60) {
+    document.write('Você é um adulto')
+} 
+
+if(idade >= 60) {
+    document.write('Você é um idoso')
+} 
+
+//feito pelo professor
+/*
+var idade = prompt('Digite a idade');
+
+if(idade >= 0 && idade < 15){
+ alert('Criança');
+}
+
+if(idade >= 15 && idade < 30){
+ alert('Jovem');
+}
+
+if(idade >= 30 && idade < 60){
+ alert('Adulto');
+}
+
+if(idade >= 60){
+ alert('Idoso');
+} 
+*/
