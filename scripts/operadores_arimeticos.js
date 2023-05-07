baf348d4854d005a7f28e4457dc56959fb5f7882
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2023-05-07 09:55:29 -0300

    Operadores aritmeticos

---
+++ scripts/operadores_arimeticos.js
@@ -0,0 +1,21 @@
+var num1 =prompt('Digite o valor de num1')
+
+var num2 =prompt('Digite o valor de num2')
+
+num1 = parseInt(num1)
+num2 = parseInt(num2)
+
+document.write('A soma entre 9 e 2 é: ' + (num1 + num2) + '<br />')
+
+document.write('A subtração entre 9 e 2 é: ' + (num1 - num2)+ '<br />')
+
+document.write('A multiplicação entre 9 e 2 é: ' + (num1 * num2)+ '<br />')
+
+document.write('A divisão entre 9 e 2 é: ' + (num1 / num2)+ '<br />')
+
+document.write('A modulo entre 9 e 2 é: ' + (num1 % num2)+ '<br />')
+
+document.write('o incremento de:'+ num1 +  'é ' + (++num1)+ '<br />')
+
+document.write('o decremento de:' + num1 +  'é ' + (--num1)+ '<br />')
+
