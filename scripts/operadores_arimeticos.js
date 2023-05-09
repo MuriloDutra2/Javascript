888f8260fec6d93976e41e0e31ba6981062634cf
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2023-05-09 05:54:35 -0300

    dicas de atribuição de valores / strings

--- scripts/operadores_arimeticos.js
+++ scripts/operadores_arimeticos.js
@@ -1,5 +1,5 @@
 //trabalho escolar
-
+/*
 var num1 =prompt('Digite o valor de num1')
 
 var num2 =prompt('Digite o valor de num2')
@@ -20,4 +20,22 @@ document.write('A modulo entre 9 e 2 é: ' + (num1 % num2)+ '<br />')
 document.write('o incremento de:'+ num1 +  'é ' + (++num1)+ '<br />')
 
 document.write('o decremento de:' + num1 +  'é ' + (--num1)+ '<br />')
+*/
 
+//var teste = 10
+
+//teste = teste + 5
+//maneira de atribuir um valor de maneira mais rapida
+//teste += 5
+//teste -= 5
+//teste *= 5
+//teste /= 5
+//teste %= 3
+
+var teste = 'ola '
+
+teste += 'jorge'
+
+
+
+document.write(teste)
