27423f2e7e2f2db9f5bfeca4b104fdd6fe6896ab
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2023-06-15 12:33:39 -0300

    DOM selecionando elementos

---
+++ scripts/dom_selecionando_elementos.js
@@ -0,0 +1,9 @@
+console.log(document.getElementById('nome_usuario'))
+
+
+console.log(document.getElementsByClassName('campo_texto'))
+
+console.log(document.getElementsByTagName('input'))
+
+console.log(document.getElementsByTagName('nome'))
+
