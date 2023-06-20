2c55198ad2de3d24a1bf8142d9e3acf239722003
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2023-06-20 05:47:27 -0300

    DOM manipulando estilos

---
+++ scripts/dom_manipulando_estilos.js
@@ -0,0 +1,5 @@
+function modificaEstilo(corDeFundo, l, a) {
+    document.getElementById('quadrado').style.background = corDeFundo
+    document.getElementById('quadrado').style.width = l
+    document.getElementById('quadrado').style.height = a
+}
