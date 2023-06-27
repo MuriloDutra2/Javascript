905d9858718c15d34148715c632e65bb63046294
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2023-06-27 10:42:08 -0300

    Array

---
+++ scripts/array_basico.js
@@ -0,0 +1,24 @@
+//var lista_frutas = Array()
+// esse codigo acima, significa a mesma coisa que o de baixo
+/*
+var lista_frutas = []
+
+lista_frutas[1] = 'Banana'
+lista_frutas[2] = true
+lista_frutas[3] = 'Morango'
+lista_frutas['x'] = 100
+
+console.log(lista_frutas['x'])
+
+console.log(lista_frutas[2])
+*/
+//Podemos escrever dessa forma tbm
+/*
+var lista_frutas = Array('banana', 'maça', 'Morango', 'Uva')
+
+console.log(lista_frutas[0])*/
+
+//e tbm dessa forma
+var lista_frutas = ['banana', 'maça', 'Morango', 'Uva']
+
+console.log(lista_frutas[2])
