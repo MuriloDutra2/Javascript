ea4933981dbcc847ae62a6639dc319c0c95425fa
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2023-07-04 08:09:11 -0300

    Tabuada com for laço encadeado

---
+++ scripts/lacos_pratica_2.js
@@ -0,0 +1,21 @@
+/*for(var x = 1; x <= 10; x++){
+    document.write('1 x ' + x + ' = ' + (1 * x) + '<br />')
+}
+
+document.write('<hr />')
+
+for(var x = 1; x <= 10; x++){
+    document.write('2 x ' + x + ' = ' + (2 * x) + '<br />')
+}
+*/
+
+
+for(var y = 1; y <= 1000; y++) {
+    for(var x = 1; x <= 10; x++){
+        document.write( y + ' x ' + x + ' = ' + (y * x) + '<br />')
+    }
+
+    document.write('<hr />')
+}
+
+
