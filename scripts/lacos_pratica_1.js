211d230ec0daf198e0e44c5f72751478c724ebe7
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2023-07-04 07:59:50 -0300

    Laços pratica 1

---
+++ scripts/lacos_pratica_1.js
@@ -0,0 +1,16 @@
+var lista_frutas = Array()
+
+lista_frutas[0] = 'Banana'
+lista_frutas[1] = 'Maça'
+lista_frutas[2] = 'Morango'
+lista_frutas[3] = 'Uva'
+
+console.log(lista_frutas.length)
+
+
+var y = 0
+
+while(y < lista_frutas.length) {
+    document.write(lista_frutas[y] +'<br />')
+    y++
+}
