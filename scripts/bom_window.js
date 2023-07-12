a3b8eb5f5243a839e874fa5d3f4f86237bd42bd8
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2023-07-12 09:19:06 -0300

    Bom window

---
+++ scripts/bom_window.js
@@ -0,0 +1,9 @@
+var janela 
+
+function abrirPopUp() {
+    janela = window.open('http://www.google.com', 'nova_janela', 'width=200', 'height=100')
+}
+
+function fecharPopUp() {
+    janela.close()
+}
