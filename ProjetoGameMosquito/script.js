2ff3cc6fa2e3f9dc086b372f51e9a51432fc722b
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2023-07-19 13:32:15 -0300

    posições randomicas

--- ProjetoGameMosquito/script.js
+++ ProjetoGameMosquito/script.js
@@ -10,3 +10,30 @@ console.log(altura, largura)
 
 }
 
+ajustaTamanhoPalcoJogo()
+
+
+function posicaoRandomica() {
+
+var posicaoX = Math.floor(Math.random() * largura) - 90
+var posicaoY =  Math.floor(Math.random() * altura) - 90
+
+posicaoX = posicaoX  < 0 ? 0 : posicaoX
+
+posicaoY = posicaoY  < 0 ? 0 : posicaoY
+
+
+console.log(posicaoX, posicaoY)
+
+// ciar o elemento html
+
+var mosquito = document.createElement('img')
+
+mosquito.src = 'img/mosca.png'
+mosquito.className = 'mosquito1'
+mosquito.style.left = posicaoX + 'px' 
+mosquito.style.top = posicaoY + 'px' 
+mosquito.style.position = 'absolute' 
+
+document.body.appendChild(mosquito)
+}
