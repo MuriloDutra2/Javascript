85cb1ce9c812825ed1d9d0a945715c21e7d7010a
Murilo Dutra <murilonunesdesouzadutra@hotmail.com>
2023-08-01 05:33:16 -0300

    VItoria

--- ProjetoGameMosquito/script.js
+++ ProjetoGameMosquito/script.js
@@ -23,7 +23,7 @@ var cronometro = setInterval(function(){
     if(tempo < 0 ) {
         clearInterval(cronometro)
         clearInterval(criaMosquito)
-        alert('Vitoria')
+        window.location.href = 'vitoria.html'
     }else {
         document.getElementById('cronometro').innerHTML = tempo
     }
